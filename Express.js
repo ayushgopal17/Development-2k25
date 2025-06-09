2ef0b308693a56ebfb605e91274f6d558b626963
Ayush gopal <ayushgopal17@gmail.com>
2025-06-09 16:11:26 +0530

    updated

--- Express.js
+++ Express.js
@@ -1,9 +1,13 @@
-const express= require("express");
+const express = require("express");
 
-const app=express();
+const app = express();
+
 app.listen(3003); 
 
+
 // then run in terminal as node Express.js
+
+// if any problem arieses then you can try "npm insatll express" comman in your terminal 
 
 
 // then go to livehost:3003
