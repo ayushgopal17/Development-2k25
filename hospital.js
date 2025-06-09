975649954261c802ee67db10d9760dd87dac192b
Ayush gopal <ayushgopal17@gmail.com>
2025-06-09 19:33:25 +0530

    put in hospital

--- hospital.js
+++ hospital.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app=express();
 
+app.use(express.json());
 const users= [{
-
+//
     name: "John",
     kidney: [{
       healthy: false
 }]
 
 }];
+
+
 
 app.get("/" ,function(req,res)
 {
@@ -36,5 +39,30 @@ res.json({
 
 
 })
+
+app.post("/",function(req,res){
+
+
+    const isHealthy=req.body.isHealthy;
+    users[0].kidney.push({
+        healthy :isHealthy
+    })
+    res.json
+    ({msg: "Done!"
+
+
+    })
+
+})
+
+app.put("/",function(req,res){
+    for(let i=0;i<users[0].kidney.length;i++)
+    {
+        users[0].kidney[i].healthy =true;
+    }
+    res.json({});
+})
+
+
 app.listen(3000);
 
