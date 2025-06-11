58046ac97be741447ef472f095db5b9c127a055a
Ayush gopal <ayushgopal17@gmail.com>
2025-06-11 18:54:48 +0530

    updated file.js

--- file.js
+++ file.js
@@ -1,1 +1,17 @@
-console.log(""welcome to my new file)
+console.log("welcome to my new file")
+
+
+let a=5;
+let b=15;
+const sum=a + b
+
+if(sum==20}
+
+{
+console.log("created a new file through terminal successfully")
+
+}
+
+else
+{
+console.log("Try again bro")}
