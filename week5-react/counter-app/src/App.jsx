31146760602e6c13c748f8d5384b223c211032a3
Ayush gopal <ayushgopal17@gmail.com>
2025-10-03 19:17:49 +0530

    dyanmic todo website

--- week5-react/counter-app/src/App.jsx
+++ week5-react/counter-app/src/App.jsx
@@ -1,36 +1,43 @@
 import { useState } from "react";
 
 function App(){
-// const[Todos,setTodos]=useState([{
-//   title : "go to gym",
-//   description : "go to gym from 7-9",
-//   completed: false
-// },
-// {
-//    title : "study DSA",
-//   description : "Study DSAfrom 9-11",
-//   completed: true
-// }]);
+const[todos,setTodos]=useState([{
+  title : "go to gym",
+  description : "go to gym from 7-9",
+  completed: false
+},
+{
+   title : "study DSA",
+  description : "Study DSAfrom 9-11",
+  completed: true
+}]);
 
+function addTodo(){
+  setTodos([...todos,{
+    title: "new todo",
+    description: "description of new todo"
+  }])
+}
 return(
   <div>
-    <Custombutton count={count} setCount={setCount}></Custombutton>
-     <Custombutton count={count+1} setCount={setCount}></Custombutton>
-      <Custombutton count={count-1} setCount={setCount}></Custombutton>
-       <Custombutton count={count*10} setCount={setCount}></Custombutton>
+    <button onClick={addTodo}>Add a random Todo</button>
+  {
+  todos.map(function(todo){
+    return <Todo title={todo.title} description={todo.description} />
+  })}
+
   </div>
+
 )
 }
 
-function Custombutton(props){
-  function onClickHandler() {
-    props.setCount(props.count+1);
+function Todo(props){
   
-}
 
-return <button onClick={onClickHandler}>
-  Counter {props.count}
-</button>
+return <div>
+  <h1>{props.title}</h1>
+  <h2>{props.description}</h2>
+</div>
 }
 
 export default App
