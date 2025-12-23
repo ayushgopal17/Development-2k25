93e2f9f31df2275efd01caf9ef8804b2009bff2a
Ayush gopal <ayushgopal17@gmail.com>
2025-12-23 23:35:14 +0530

    enum

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -1,27 +1,11 @@
-interface User {
-    firstName: String;
-    LastName: string;
-    age:number;
-    email?:String
+enum Direction {
+    Up="UP",
+    Down="DOWN",
+    left="LEFT",
+    rigth="RIGHT"
 }
+function doSomething(KeyPressed : Direction){
 
-function isLegal(user:User){
-    if(user.age>18){
-        return true;
-    }
-    else {
-        return false;
-    }
 }
-
-function greet(user:User){
-    console.log("Hi there"+ user.firstName);
-}
-   isLegal({
-    firstName: "Ayush",
-    LastName: "Gopal",
-    age: 23
-   })
-
-   let ans=greet;
-   console.log(ans);
+doSomething(Direction.Down)
+console.log(Direction.Down)
