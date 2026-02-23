fd0e136ae3c5dbcda39f7a06cc7c8f333fd8d54f
Ayush gopal <ayushgopal17@gmail.com>
2026-02-23 19:32:14 +0530

    backend in nextjs

---
+++ next-backend/app/api/user/route.ts
@@ -0,0 +1,7 @@
+export function GET(){
+    return Response.json({
+        email:"Ayush@gmail.com",
+        name: "Ayush"
+    })
+
+}
---
+++ next-backend/app/page.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+async function getUserData() {
+  await new Promise((r)=> setTimeout(r,5000))
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
+}
+
+export default async function Home() {
+
+  const userData=await getUserData();
+
+  return (
+   <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+
+  );
+}
