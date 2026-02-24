6e343236715432839b1d732c14fe712ffe2a168e
Ayush gopal <ayushgopal17@gmail.com>
2026-02-25 00:06:40 +0530

    adding Signup

--- next-backend/app/api/user/route.ts
+++ next-backend/app/api/user/route.ts
@@ -1,7 +1,43 @@
-export function GET(){
-    return Response.json({
-        email:"Ayush@gmail.com",
-        name: "Ayush"
+// export function GET(){
+//     return Response.json({
+//         email:"Ayush@gmail.com",
+//         name: "Ayush"
+//     })
+
+// }
+
+// export function POST(){
+//     return Response.json({
+//         email:"Ayush@gmail.com",
+//         name: "Ayush"
+//     })
+
+// }
+// export function PUT(){
+//     return Response.json({
+//         email:"Ayush@gmail.com",
+//         name: "Ayush"
+//     })
+ 
+// }
+
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { DefaultDeserializer } from "v8";
+import { Dawning_of_a_New_Day } from "next/font/google"
+
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest){
+    const body= await req.json();
+    await client.user.create({
+        data:{
+            username : body.name,
+            password: body.password
+        }
     })
 
+    return Response.json({
+        message: "You are logged in"
+    })
 }
