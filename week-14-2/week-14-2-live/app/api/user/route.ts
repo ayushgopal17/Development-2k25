aebdbe0ca8d63e6dfb88365943bed47d7bc2e2a7
Ayush gopal <ayushgopal17@gmail.com>
2026-03-12 20:41:07 +0530

    route in next

--- week-14-2/week-14-2-live/app/api/user/route.ts
+++ week-14-2/week-14-2-live/app/api/user/route.ts
@@ -1,6 +1,19 @@
-export function GET(){
-    return Response.json({
-        email:"Ayush@gmail.com",
-        name:"Ayush"
-    })
+import { NextRequest } from "next/server";
+
+export async function GET() {
+  return Response.json({
+    name: "Ayush",
+    email: "ayush@gmail.com"
+  });
+}
+
+export async function POST(req: NextRequest){
+
+    const body= await req.json();
+ console.log(body);
+
+  return Response.json({
+    message: "you are logged in!"
+  })
+
 }
