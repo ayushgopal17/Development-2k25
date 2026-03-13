459b3ff8648d4540e0ee2de726a6ba44ad00ea4e
Ayush gopal <ayushgopal17@gmail.com>
2026-03-13 17:43:14 +0530

    next-js-with-postgres

--- week-14-2/week-14-2-live/app/api/user/route.ts
+++ week-14-2/week-14-2-live/app/api/user/route.ts
@@ -1,4 +1,12 @@
 import { NextRequest } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const client = new PrismaClient({ adapter });
 
 export async function GET() {
   return Response.json({
@@ -10,6 +18,12 @@ export async function GET() {
 export async function POST(req: NextRequest){
 
     const body= await req.json();
+   await client.user.create({
+        data:{
+            username: body.username,
+            password: body.password
+        }
+    })
  console.log(body);
 
   return Response.json({
