bd59cff48c062a35c08eab3819f25e527a7240fb
Ayush gopal <ayushgopal17@gmail.com>
2026-04-07 19:52:25 +0530

    routes

--- crud/index.js
+++ crud/index.js
@@ -1,24 +1,60 @@
-const express = require ('express')
+const express =require('express')
 const app=express()
 
-app.use(express.json())
-
-let todos=[]
-
-app.post('/todos',(req,res)=>{
-    const { task }= req.body
-
-    const newTodo={
-        id: Date.now(),
-        task
-    }
-
-    todos.push(newTodo)
-    res.json(newTodo)
+app.get("/",(req,res)=>{
+    res.sendFile("/Users/ayushgopal/Development-2k25/crud/index.html")
 })
 
-app.get('/todos',(req,res) =>{
-    res.json(todos)
+app.post('/sum',function(req,res){
+
+    const a=parseInt(req.body.a);
+    const b=parseInt(req.body.b);
+
+    const sum=a+b;
+    res.json({
+        sum
+})
+
+})
+
+app.get('/sub/:a/:b',(req,res)=>{
+
+    const a=req.params.a;
+    const b=req.params.b;
+
+    sub=a-b;
+
+    res.json({
+        sub
+})
+})
+
+app.get('/',(req,res)=> {
+res.sendFile("/Users/ayushgopal/Development-2k25/crud/index.html")
+})
+
+
+app.get('/mul/:a/:b',(req,res)=>{
+ 
+const a=req.params.a;
+const b=req.params.b;
+
+mul=a*b;
+res.json({
+    mul
+})
+})
+
+app.get('/div/:a/:b',(req,res)=>{
+a=req.params.a;
+b=req.params.b;
+const div=a/b;
+
+res.send("<b><u>" +
+div
++
+"</b></u>"
+)
 })
 
 app.listen(3000)
